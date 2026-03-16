f513ddf9cd4d599b0a11eb85560f3f8120748fcc
Anastasiia Burykh <burykh_2000@mail.ru>
2026-03-16 21:06:55 +0300

    update urls media

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,7 +27,6 @@ urlpatterns = [
 ]
 
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
